b892558e596144aa3772c414ae306b2b22c834e8
Tak <Tak@DESKTOP-QCEGMGQ>
2021-09-03 02:20:17 +0900

    コミット・プッシュ（2021/9/2時点）

--- src/info/books/Init_edit.java
+++ src/info/books/Init_edit.java
@@ -1,5 +1,35 @@
 package info.books;
 
-public class Init_edit {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Init_edit extends HttpServlet {
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException,IOException{
+		
+		// 初期設定
+				String seqid = null;
+				String message = null;
+				
+				request.setCharacterEncoding("UTF-8");
+				
+				try {			
+					// seqIDをセット
+					seqid = (String) request.getAttribute("seqid");
+
+					request.getRequestDispatcher("/book_edit.jsp").forward(request, response);
+					
+				}catch(Exception e) {
+					// 例外処理
+					message ="エラーが発生しました。再度ログインし直してください。";
+					request.setAttribute("message", message);
+					request.getRequestDispatcher("/books_login.jsp").forward(request, response);
+				}
+	}
 }
