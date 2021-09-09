79be63e87fe50a859edb5f5ccc6777de143ab158
Tak <Tak@DESKTOP-QCEGMGQ>
2021-09-10 01:49:44 +0900

    コミット・プッシュ（2021/9/9時点）

---
+++ WebContent/js/logout.js
@@ -0,0 +1,10 @@
+function onLogoutButton(){
+    var flag = confirm("ログアウトしますか？");
+
+    // 「はい」が押されたときの処理
+    if ( flag == true ){
+    }else{
+    	// 「いいえ」が押されたときの処理
+    	return false;
+    }
+};
